c66c78618edaef023f3f4b7d13ac6936a9effecd
Rohit Mehra <rohitmehra.cs17@bmsce.ac.in>
2018-06-21 14:38:29 +0530

    fix contact-form

--- mail.php
+++ mail.php
@@ -1,13 +1,8 @@
 <?php
-if(isset( $_POST['name']))
-  $name = $_POST['name'];
-if(isset( $_POST['email']))
-  $email = $_POST['email'];
-if(isset( $_POST['message']))
-  $message = $_POST['message'];
-if(isset( $_POST['subject']))
-  $subject = $_POST['subject'];
-
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
 $content="From: $name \n Email: $email \n Message: $message";
 $recipient = "rohitmehra.cs17@gmail.com";
 $mailheader = "From: $email \r\n";
